30f9b4a9a75c1a2234e5decbede4a2390cd39d48
MaccSob <71sobmaciej@gmail.com>
2025-03-30 23:53:41 +0200

    cleaning stuff up

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
 require('dotenv').config()
 const path = require("node:path");
-const { Pool } = require("pg");
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const indexRouter = require("./routes/indexRouter");
 const LocalStrategy = require('passport-local').Strategy;
+const pool = require("./db/pool");
 const PORT = process.env.PORT;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_USER = process.env.DB_USER;
-const DB_HOST = process.env.DB_HOST;
-const DB_PORT = process.env.DB_PORT;
-const DB_NAME = process.env.DB_NAME;
-
-const pool = new Pool({
-    host: DB_HOST, 
-  user: DB_USER,
-  database: DB_NAME,
-  password: DB_PASSWORD,
-  port: DB_PORT
-  });
-  
 
 const app = express();
 
@@ -32,7 +18,6 @@ app.set("view engine", "ejs");
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
-
 
 
 
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const { Router } = require("express");
 const indexRouter = Router();
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
 
 
 
@@ -10,19 +12,5 @@ indexRouter.get("/", (req, res) => res.render("index"));
 indexRouter.get("/signup", (req, res) => res.render("signup"));
 indexRouter.get("/login", (req, res) => res.render("login"));
 
-indexRouter.post("/signup", async (req, res, next) => {
-    try {
-      await pool.query("INSERT INTO users (firstname, lastname, username,email,password) VALUES ($1, $2, $3, $4, $5)", [
-        req.body.firstname,
-        req.body.lastname,
-        req.body.email,
-        req.body.username,
-        req.body.password,
-      ]);
-      res.redirect("/login");
-    } catch(err) {
-      return next(err);
-    }
-  });
 
   module.exports = indexRouter;
