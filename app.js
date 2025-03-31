ceb37f8f3175476c52abb5ab87a053c77bbb2752
MaccSob <71sobmaciej@gmail.com>
2025-03-31 23:40:21 +0200

    added passport

--- app.js
+++ app.js
@@ -20,5 +20,59 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 
+app.post("/signup", async (req, res, next) => {
+  try {
+    await pool.query("INSERT INTO users (firstname, lastname, username, email, password) VALUES ($1, $2, $3, $4, $5)", [
+      req.body.firstname,
+      req.body.lastname,
+      req.body.username,
+      req.body.email,
+      req.body.password,
+    ]);
+    res.redirect("/");
+  } catch(err) {
+    return next(err);
+  }
+});
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+})
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
 
 app.listen(PORT, () => console.log(`app listening on port ! ${PORT}`));
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,14 +1,11 @@
 const express = require("express");
+const path = require("node:path");
 const app = express();
 const { Router } = require("express");
-const indexRouter = Router();
-const passport = require("passport");
-const LocalStrategy = require('passport-local').Strategy;
+const indexRouter = Router();;
+;
 
-
-
-
-indexRouter.get("/", (req, res) => res.render("index"));
+indexRouter.get("/", (req, res) => res.render("index", {user: req.user}));
 indexRouter.get("/signup", (req, res) => res.render("signup"));
 indexRouter.get("/login", (req, res) => res.render("login"));
 
